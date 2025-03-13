eeadcbf4aaab4161cb6f638ef64621a58516c9cc
LindaTancini <lindatancini95@gmail.com>
2025-03-13 14:36:45 +0100

    feat: added API

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Ciao Linda");
+
+// DICHIARO I DATI DELL'API
+axios
+  .get("https://flynn.boolean.careers/exercises/api/random/mail")
+  .then((response) => {
+    const email = response.data.response;
+    console.log(email);
+  });

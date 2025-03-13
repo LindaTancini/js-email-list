17f56ce408b45774145bffd53ac28ee599819d82
LindaTancini <lindatancini95@gmail.com>
2025-03-13 14:46:09 +0100

    feat: added (cycle for), for generate 10 email

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 console.log("Ciao Linda");
 
 // DICHIARO I DATI DELL'API
-axios
-  .get("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then((response) => {
-    const email = response.data.response;
-    console.log(email);
-  });
+//METTENDO LE API DENTRO UN CICLO FOR, GENERO 10 EMAIL
+for (let i = 0; i < 10; i++) {
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((response) => {
+      const email = response.data.response;
+      console.log(email);
+    });
+}
